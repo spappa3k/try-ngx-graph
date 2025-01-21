0f77e734eb7720eafccf8f9cd9bcac853aef806a
spappa3k <pappalardo.dev@gmail.com>
2025-01-21 13:46:30 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,13 @@ export class AppComponent {
   nodes = [
     { id: 'start', label: 'Start' },
     { id: 'process', label: 'Process' },
+    {id : 'pippo', label:'Pippo'},
     { id: 'end', label: 'End' }
   ];
 
   links = [
-    { source: 'start', target: 'process', label: 'to Process' },
+    { source: 'start', target: 'process', label: 'to Pippo' },
+    {source:'pippo', target:'process', label:'to Process'},
     { source: 'process', target: 'end', label: 'to End' }
   ];
 }
